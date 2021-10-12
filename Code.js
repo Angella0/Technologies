00bda1bcfb097991a46234bd5d4618b8a4d522fe
Angella0 <angellasimbwa@gmail.com>
2021-10-12 12:30:39 +0300

    push

---
+++ Code.js
@@ -0,0 +1,12 @@
+document.getElementById("about").style.color = "blue"
+document.getElementById("abt").style.color = "blue"
+document.getElementById("abut").style.color = "blue"
+
+// function myPage(){
+//     var a = document.getElementById("myNav");
+
+//     if( a.className === "nav"){
+//         a.className +=
+//     }
+
+// }
